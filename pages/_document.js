7e927849070f3a6cee0566dc766011562c1f1aa9
Abdul Rehman <arrehman0r@gmail.com>
2024-03-10 22:05:56 +0500

    owl only cdn

--- pages/_document.js
+++ pages/_document.js
@@ -1,5 +1,5 @@
 import Document, { NextScript, Head, Main, Html } from "next/document";
-
+import Script from "next/script";
 export default class MyDocument extends Document {
   static async getInitialProps(ctx) {
     const initialProps = await Document.getInitialProps(ctx);
@@ -20,7 +20,7 @@ export default class MyDocument extends Document {
 
           <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 
-          {/* <link
+          <link
             rel="stylesheet"
             type="text/css"
             href="https://d-themes.com/react/riode/demo-1/vendor/riode-fonts/riode-fonts.css"
@@ -29,18 +29,18 @@ export default class MyDocument extends Document {
             rel="stylesheet"
             type="text/css"
             href="https://d-themes.com/react/riode/demo-1/vendor/fontawesome-free/css/all.min.css"
-          /> */}
+          />
           <link
             rel="stylesheet"
             type="text/css"
-            href="https://d-themes.com/react/riode/demo-1/vendor/owl-carousel/owl.carousel.min.css"
+            href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css"
           />
         </Head>
 
         <body>
           <Main />
 
-          <script src="https://d-themes.com/react/riode/demo-1/js/jquery.min.js"></script>
+          {/* <script src="https://d-themes.com/react/riode/demo-1/js/jquery.min.js"></script> */}
 
           <NextScript />
         </body>
